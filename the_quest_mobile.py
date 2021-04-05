4c2dc8cdd2fad35550d89b2d3119c46212510fe6
MetabaronGit <76850166+MetabaronGit@users.noreply.github.com>
2021-04-05 18:06:08 +0200

    05-02

--- the_quest_mobile.py
+++ the_quest_mobile.py
@@ -7,14 +7,15 @@ LINE_SPACING_PX = 5
 
 
 class Player:
-    def __init__(self, name:str, hp_max, combat, munition, command, credits):
+    def __init__(self, name: str, hp_max, combat, munition, command, credits, tab_lines):
         self.__name = name
         self.__stats = {"HP": str(hp_max),
                         "HP_MAX": str(hp_max),
                         "COMBAT": str(combat),
                         "MUNITION": str(munition),
                         "COMMAND": str(command),
-                        "CR": str(credits)}
+                        "CR": str(credits),
+                        "tab_lines": str(tab_lines)}
 
     def get_value(self, key: str):
         if self.__stats.get(key):
@@ -23,15 +24,19 @@ class Player:
             return ""
 
 class Card:
-    def __init__(self, name:str, condition="", image="cultGreen", image_shift=0):
+    def __init__(self, name:str, type="event", condition="", image="cultGreen", image_shift=0):
         self.__name = name
         self.__condition = condition
         self.__actions = ["take MUNITION", "enlarge COMBAT", "pray for Omnisiah", "SCORE"]
         self.__image = "images/" + image + ".png"
         self.__image_shift = image_shift
+        self.__type = type
 
     def get_name(self):
         return self.__name
+
+    def get_type(self):
+        return self.__type
 
     def get_image(self):
         return self.__image
@@ -45,7 +50,12 @@ class Card:
 
 card1 = Card("Forge supply depot", condition="Mechanic heroes have doubled all effects here.", image="enemy03", image_shift=40)
 card2 = Card("Mechanicum sanctuary", "All Mechanicum heroes increase MAX HP by 2.")
-player = Player("Jurgen XVII", 20, 0, 0, 0, 10)
+player = Player("servitor XVII-12458", 20, 0, 0, 0, 10, tab_lines=4)
+
+
+def create_quest_deck():
+    return list[card1, card2]
+
 
 pygame.init()
 pygame.font.init()
@@ -65,15 +75,20 @@ CONSOLE_FONT_WIDTH_PX, CONSOLE_FONT_HEIGHT_PX = console_font.size("-")
 actual_action = 0
 
 
-def draw_image(y):
-    y = y - card1.get_image_shift()
+def draw_card(card):
+    text = f"=< {card.get_name()} >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - len(card.get_name()))
+    y = 0
+    draw_text(text, y, color=dark_green)
+    # y = y - card1.get_image_shift()
     # draw_text("Forge supply depot", y)
-    draw_text(card1.get_name(), y)
-    testImg = pygame.image.load(card1.get_image())
+    card_img = pygame.image.load(card1.get_image())
     # obrazek vycentrovany na ose x
-    screen.blit(testImg, (SCREEN_WIDTH_PX // 2 - testImg.get_width() // 2, y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX))
-    draw_text(card1.get_condition(), y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX * 2 + testImg.get_height())
+    screen.blit(card_img, (SCREEN_WIDTH_PX // 2 - card_img.get_width() // 2, y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX))
+    draw_text(card1.get_condition(), y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX * 2 + card_img.get_height())
 
+    text = f"=< {card.get_type()} >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - len(card.get_type()))
+    y = 0
+    draw_text(text, y, color=dark_green)
 
 def draw_text(text_string, y, x=0, color=TEXT_COLOR, background_color=None, center=False):
     text = console_font.render(text_string, True, color, background_color)
@@ -86,22 +101,29 @@ def draw_text(text_string, y, x=0, color=TEXT_COLOR, background_color=None, cent
     screen.blit(text, text_rect)
 
 
-def draw_cursor(y):
+def draw_cursor(actual_action, options: list):
+    y = SCREEN_HEIGHT_PX - (int(player.get_value("tab_lines")) * 2 + 4) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
+    y -= (len(options) + 1) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
     draw_text(" " * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX),
               y + (actual_action + 1) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX), background_color=CURSOR_COLOR)
 
-def draw_console(y):
-    options = ["take MUNITION", "enlarge COMBAT", "pray for Omnisiah", "SCORE", "None"]
-    draw_text("-" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX), y)
+def draw_console(player):
+    options = ["take MUNITION", "enlarge COMBAT", "pray for Omnisiah", "SCORE", "None", "special hero action"]
+    y = SCREEN_HEIGHT_PX - (int(player.get_value("tab_lines")) * 2 + 4) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
+    y -= (len(options) + 1) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
+
+    text = "=< actions >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - 12)
+    draw_text(text, y, color=dark_green)
+
     for i, item in enumerate(options, 1):
-        draw_text(f" [{i}] {item}", y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * i, center=False)
+        draw_text(f" [{i}] {item}", y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * i)
 
 
 def draw_player_stats(player):
     tab_columns = 3  # při změně šířky SCREEN_WIDTH se mění jen šířka sloupců
     tab_column_width_chars = SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX // tab_columns
 
-    tab_lines = 4  # muzeme pridavat ci ubirat podle poctu zobrazovanych hodnot
+    tab_lines = int(player.get_value("tab_lines"))  # muzeme pridavat ci ubirat podle poctu zobrazovanych hodnot
 
     # umisteni se pocita od spodniho okraje SCREENu nad popisky vybranych akci
     y = SCREEN_HEIGHT_PX - (tab_lines * 2 + 4) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
@@ -172,9 +194,9 @@ while not game_over:
 
 
             screen.fill((0, 0, 0))
-            draw_image(50)
-            draw_cursor(320)
-            draw_console(320)
+            draw_card(card1)
+            draw_cursor(actual_action, [1,2,3,4,5,6])
+            draw_console(player)
             draw_player_stats(player)
             draw_text("add 2 to your combat bonus", SCREEN_HEIGHT_PX - (FONT_SIZE_PX + LINE_SPACING_PX) * 2, color=white,
                       center=True)
