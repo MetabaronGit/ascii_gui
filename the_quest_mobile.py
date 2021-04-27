979aa494861131ebda0af2fead54a5082dae74b2
rad <cepelka.jr@email.cz>
2021-04-27 13:38:56 +0200

    def main

--- the_quest_mobile.py
+++ the_quest_mobile.py
@@ -1,31 +1,28 @@
 import pygame
 
+pygame.init()
+pygame.font.init()
+
+FONT_SIZE_PX = 18
+CONSOLE_FONT = pygame.font.Font('lucon.ttf', FONT_SIZE_PX)
+CONSOLE_FONT_WIDTH_PX, CONSOLE_FONT_HEIGHT_PX = CONSOLE_FONT.size("-")
+LINE_SPACING_PX = 5
+CONSOLE_FONT_WIDTH_PX = int
+CONSOLE_FONT_HEIGHT_PX = int
+
 SCREEN_WIDTH_PX = 540  # original=540
 SCREEN_HEIGHT_PX = 780  # original=960
-FONT_SIZE_PX = 18
-LINE_SPACING_PX = 5
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX))
 
-
-class Player:
-    def __init__(self, name: str, hp_max, combat, munition, command, credits, tab_lines):
-        self.__name = name
-        self.__stats = {"HP": str(hp_max),
-                        "HP_MAX": str(hp_max),
-                        "COMBAT": str(combat),
-                        "MUNITION": str(munition),
-                        "COMMAND": str(command),
-                        "CR": str(credits),
-                        "tab_lines": str(tab_lines)}
-
-    def get_value(self, key: str):
-        if self.__stats.get(key):
-            return self.__stats.get(key)
-        else:
-            return ""
+TEXT_COLOR = (0, 200, 0)
+WHITE = (255, 255, 255)
+DARK_GREEN = (0, 80, 0)
+CURSOR_COLOR = (0, 100, 0)
 
 
 class PlayerValue:
-    def __init__(self, name: str, value: int, max_value=-1):
+    # name="NONE" -> nepouziva se, value= -1 -> unlimited, max_value=-1 -> unlimited
+    def __init__(self, name: str, value=0, max_value=-1):
         self.__name = name
         self.__value = value
         self.__max_value = max_value
@@ -45,14 +42,44 @@ class PlayerValue:
             self.__value = self.__max_value
 
     def decrease_value(self, amount: int) -> None:
-        self.__value -= amount
-        if self.__value < 0:
-            self.__value = 0
+        if self.__value != -1:
+            self.__value -= amount
+            if self.__value < 0:
+                self.__value = 0
 
 
+class Player:
+    def __init__(self, name: str):
+        if name == "Necron Lord":
+            self.__var1 = PlayerValue("POWER", 20, 20)
+            self.__var2 = PlayerValue("COMBAT")
+            self.__var3 = PlayerValue("MUNITION", 2)
+            self.__var4 = PlayerValue("COMMAND", 4)
+            self.__var5 = PlayerValue("FORTIFICATION")
+            self.__var6 = PlayerValue("VP")
+            self.__var7 = PlayerValue("NONE", -1)
+            self.__var8 = PlayerValue("SUPPLY", 5)
+            self.__var9 = PlayerValue("DECK", 10, 10)
+
+        self.__stats = dict()
+        self.__stats[self.__var1.get_name()] = self.__var1
+        self.__stats[self.__var2.get_name()] = self.__var2
+        self.__stats[self.__var3.get_name()] = self.__var3
+        self.__stats[self.__var4.get_name()] = self.__var4
+        self.__stats[self.__var5.get_name()] = self.__var5
+        self.__stats[self.__var6.get_name()] = self.__var6
+        self.__stats[self.__var7.get_name()] = self.__var7
+        self.__stats[self.__var8.get_name()] = self.__var8
+        self.__stats[self.__var9.get_name()] = self.__var9
+        self.__stats["name"] = name
+        self.__stats["tab_lines"] = 3
 
 
-
+    def get_value(self, key: str):
+        if self.__stats.get(key):
+            return self.__stats.get(key)
+        else:
+            return ""
 
 
 class Card:
@@ -80,67 +107,44 @@ class Card:
         return self.__image_shift
 
 
-card1 = Card("Card one name")
-card2 = Card("Card two name", image="img_02.jpg")
-card3 = Card("Card three name", image="img_03.jpg")
-player = Player("servitor XVII-12458", 20, 0, 0, 0, 10, tab_lines=3)
-
-var1 = PlayerValue("HP", 20)
-
-
 def create_quest_deck():
-    return list[card1, card2]
-
-
-pygame.init()
-pygame.font.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX))
-pygame.display.set_caption("the quest - technological demo, 04.2021")
-
-
-console_font = pygame.font.Font('lucon.ttf', FONT_SIZE_PX)
-TEXT_COLOR = (0, 200, 0)
-white = (255, 255, 255)
-dark_green = (0, 80, 0)
-CURSOR_COLOR = (0, 100, 0)
-
-CONSOLE_FONT_WIDTH_PX, CONSOLE_FONT_HEIGHT_PX = console_font.size("-")
-
-actual_action = 0
+    pass
 
 
 def draw_card(card):
-    text = f"=< {card.get_name()} >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - len(card.get_name()))
+    text = card.get_name()
     y = 0
-    draw_text(text, y, color=dark_green)
+    draw_text(text, y, color=DARK_GREEN)
     # y = y - card1.get_image_shift()
     # draw_text("Forge supply depot", y)
     card_img = pygame.image.load(card.get_image())
     # obrazek vycentrovany na ose x
-    screen.blit(card_img, (SCREEN_WIDTH_PX // 2 - card_img.get_width() // 2, y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX))
+    SCREEN.blit(card_img, (SCREEN_WIDTH_PX // 2 - card_img.get_width() // 2, y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX))
     draw_text(card.get_condition(), y + CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX * 2 + card_img.get_height())
 
     text = f"=< {card.get_type()} >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - len(card.get_type()))
     y = 0
-    draw_text(text, y, color=dark_green)
+    draw_text(text, y, color=DARK_GREEN)
 
+
+# OK
 def draw_text(text_string, y, x=0, color=TEXT_COLOR, background_color=None, center=False):
-    text = console_font.render(text_string, True, color, background_color)
-    text_rect = text.get_rect()
+    text = CONSOLE_FONT.render(text_string, True, color, background_color)
+    text_rect = text.get_rect(center=(SCREEN_WIDTH_PX // 2, y))
     text_rect.y = y
-    if center:
-        text_rect.x = SCREEN_WIDTH_PX // 2 - (len(text_string) * CONSOLE_FONT_WIDTH_PX) // 2
-    else:
+
+    if not center:
         text_rect.x = x
-    screen.blit(text, text_rect)
+
+    SCREEN.blit(text, text_rect)
 
 
-def draw_cursor(actual_action, options: list):
+def draw_cursor(player: Player, actual_action, options: list):
     y = SCREEN_HEIGHT_PX - (int(player.get_value("tab_lines")) * 2 + 4) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
     y -= (len(options) + 1) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
     draw_text(" " * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX),
               y + (actual_action + 1) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX), background_color=CURSOR_COLOR)
+
 
 def draw_console(player):
     options = ["take MUNITION", "enlarge COMBAT", "pray for Omnisiah", "SCORE", "None", "special hero action"]
@@ -148,7 +152,7 @@ def draw_console(player):
     y -= (len(options) + 1) * (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX)
 
     text = "=< actions >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - 12)
-    draw_text(text, y, color=dark_green)
+    draw_text(text, y, color=DARK_GREEN)
 
     for i, item in enumerate(options, 1):
         draw_text(f" [{i}] {item}", y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * i)
@@ -165,17 +169,17 @@ def draw_player_stats(player):
 
     # draw empty stats tab
     text = "=< stats >" + "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - 10)
-    draw_text(text, y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX), color=dark_green)
+    draw_text(text, y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX), color=DARK_GREEN)
 
     free_fields = f"{'':>{tab_column_width_chars}}|{'':>{tab_column_width_chars}}|"
     separator = f"{'':->{tab_column_width_chars}}+{'':->{tab_column_width_chars}}+" + \
                 "-" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX - tab_column_width_chars * 2 - 2)
 
     for i in range(2, tab_lines * 2 + 1, 2):
-        draw_text(free_fields, y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * i, color=dark_green)
+        draw_text(free_fields, y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * i, color=DARK_GREEN)
         if i == tab_lines * 2:
             separator = "=" * (SCREEN_WIDTH_PX // CONSOLE_FONT_WIDTH_PX)
-        draw_text(separator, y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * (i + 1), color=dark_green)
+        draw_text(separator, y + (CONSOLE_FONT_HEIGHT_PX + LINE_SPACING_PX) * (i + 1), color=DARK_GREEN)
 
     # draw stats values
     player_values = [["HP", "COMBAT", "MUNITION"],
@@ -203,45 +207,62 @@ def draw_player_stats(player):
             x += tab_column_width_chars * CONSOLE_FONT_WIDTH_PX
 
 
-game_over = False
+class GameTable:
+    def __init__(self):
+        self.__name = "name"
 
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE:
-                game_over = True
 
-            if event.key == pygame.K_DOWN:
-                actual_action += 1
-                if actual_action > 4:
+def main():
+    pygame.display.set_caption("the quest - technological demo, since 04.2021")
+
+    card1 = Card("Card one name")
+    card2 = Card("Card two name", image="img_02.jpg")
+    card3 = Card("Card three name", image="img_03.jpg")
+    player = Player("Necron Lord")
+
+    actual_action = 0
+    game_over = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    game_over = True
+
+                if event.key == pygame.K_DOWN:
+                    actual_action += 1
+                    if actual_action > 4:
+                        actual_action = 0
+
+                if event.key == pygame.K_UP:
+                    actual_action -= 1
+                    if actual_action < 0:
+                        actual_action = 4
+
+                if event.key == pygame.K_1 or event.key == pygame.K_KP1:  # NUM 1
                     actual_action = 0
 
-            if event.key == pygame.K_UP:
-                actual_action -= 1
-                if actual_action < 0:
-                    actual_action = 4
+                if event.key == pygame.K_2 or event.key == pygame.K_KP2:  # NUM 2
+                    actual_action = 1
 
-            if event.key == pygame.K_1 or event.key == pygame.K_KP1:
-                actual_action = 0
+                SCREEN.fill((0, 0, 0))
+                # draw_card(card1)
+                # draw_cursor(player, actual_action, [1, 2, 3, 4, 5, 6])
+                # draw_console(player)
+                # draw_player_stats(player)
+                # draw_text("add 2 to your combat bonus", SCREEN_HEIGHT_PX - (FONT_SIZE_PX + LINE_SPACING_PX) * 2,
+                #           color=WHITE, center=True)
+                draw_text("second line of text", SCREEN_HEIGHT_PX - (FONT_SIZE_PX + LINE_SPACING_PX), color=WHITE,
+                          center=True)
 
-            if event.key == pygame.K_2 or event.key == pygame.K_KP2:
-                actual_action = 1
+            # zaviraci ikona okna X
+            if event.type == pygame.QUIT:
+                game_over = True
+
+        pygame.display.flip()
+        # pygame.display.update()
+        # clock.tick(60)
 
 
-            screen.fill((0, 0, 0))
-            draw_card(card1)
-            draw_cursor(actual_action, [1,2,3,4,5,6])
-            draw_console(player)
-            draw_player_stats(player)
-            draw_text("add 2 to your combat bonus", SCREEN_HEIGHT_PX - (FONT_SIZE_PX + LINE_SPACING_PX) * 2, color=white,
-                      center=True)
-            draw_text("second line of text", SCREEN_HEIGHT_PX - (FONT_SIZE_PX + LINE_SPACING_PX), color=white,
-                      center=True)
-
-        # zaviraci ikona okna X
-        if event.type == pygame.QUIT:
-            game_over = True
-
-    pygame.display.flip()
-    # pygame.display.update()
-    # clock.tick(60)
+if __name__ == "__main__":
+    main()
